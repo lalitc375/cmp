e204042623cd946cee90a0c70217523a614ca43c
gm-20 <rock.gm.9755@gmail.com>
2017-04-02 09:09:49 +0530

    Update login.php

--- bin/login.php
+++ bin/login.php
@@ -3,19 +3,20 @@ include("serverInfo.php");
 $connection = new mysqli($db_hostname,$db_username,$db_password,$db_database);
 if($connection->connect_error) die($connection->connect_error);
 session_start();
-	
+
 
 $args = json_decode(file_get_contents("php://input"));
 if($args->email_id!=""  && $args->password!="")
-{	
+{
 	$useremail=$args->email_id;
 	$password=$args->password;
 	$query="select * from user where email_id='$useremail'and password='$password'";
-	$run=$connection->query($query);	
-	
+	$run=$connection->query($query);
+
 	if($run->num_rows==1)
 	{
 		$row=$run->fetch_assoc();
+		$_SESSION['user_id'] = $row['user_id'];
 		$_SESSION['email_id']=$row['email_id'];
 		$_SESSION['mobile_number']=$row['mobile_number'];
 		$arr = array('loginstatus' => 1);
